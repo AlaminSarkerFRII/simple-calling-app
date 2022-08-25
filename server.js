2b2cd21f12a3c93d71af1e644e380b51b0b87b95
alamin <alamin.education1997@gmail.com>
2022-08-25 23:39:18 +0600

    setup file for heroku

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ io.on('connection', socket => {
 
 });
 
+// step : 3
+
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static("client/build"))
+}
+
 server.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
 
 
