6d80c1194c8cc3b68ea53913d01c7145b0f0f465
alamin <alamin.education1997@gmail.com>
2022-08-25 23:50:06 +0600

    setup fileu

--- server.js
+++ server.js
@@ -44,11 +44,8 @@ io.on('connection', socket => {
             users[roomID] = room;
         }
     });
-
 });
-
 // step : 3
-
 if(process.env.NODE_ENV=="production"){
     app.use(express.static("client/build"))
 }
